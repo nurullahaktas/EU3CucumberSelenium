4120c91d0e8353bfcd0203c40c09cbf804529389
CybertekSchool <github@cybertekschool.com>
2020-11-12 13:19:58 -0500

    Db Testing scenario updated

--- src/test/java/com/vytrack/step_definitions/ContactsStepDefs.java
+++ src/test/java/com/vytrack/step_definitions/ContactsStepDefs.java
@@ -1,6 +1,7 @@
 package com.vytrack.step_definitions;
 
 import com.vytrack.pages.BasePage;
+import com.vytrack.pages.ContactsPage;
 import com.vytrack.pages.DashboardPage;
 import com.vytrack.pages.LoginPage;
 import com.vytrack.utilities.BrowserUtils;
@@ -69,15 +70,18 @@ public class ContactsStepDefs {
 
 
     @When("the user clicks the {string} from contacts")
-    public void the_user_clicks_the_from_contacts(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void the_user_clicks_the_from_contacts(String email) {
+
+        BrowserUtils.waitFor(2);
+
+        ContactsPage contactsPage = new ContactsPage();
+        contactsPage.getContactEmail(email).click();
+
     }
 
     @Then("the information should be same with database")
     public void the_information_should_be_same_with_database() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        BrowserUtils.waitFor(3);
     }
 
 
