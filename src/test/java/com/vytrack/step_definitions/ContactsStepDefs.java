3b935b5ebbf37d6d664851bbd1a7ec06912e5a31
CybertekSchool <github@cybertekschool.com>
2020-11-12 16:15:27 -0500

    Db Testing Scenario Outlined added

--- src/test/java/com/vytrack/step_definitions/ContactsStepDefs.java
+++ src/test/java/com/vytrack/step_definitions/ContactsStepDefs.java
@@ -111,7 +111,7 @@ public class ContactsStepDefs {
         System.out.println("expectedEmail = " + expectedEmail);
 
 
-        //assertion
+        //assertion, Compare UI against to DB
         Assert.assertEquals(expectedFullName,actualFullName);
         Assert.assertEquals(expectedEmail,actualEmail);
         Assert.assertEquals(expectedPhone,actualPhone);
@@ -120,6 +120,46 @@ public class ContactsStepDefs {
 
     }
 
+    @Then("the information for {string} should be same with database")
+    public void the_information_for_should_be_same_with_database(String email) {
+        //get information from UI
+        ContactInfoPage contactInfoPage = new ContactInfoPage();
+        String actualFullName = contactInfoPage.contactFullName.getText();
+        String actualEmail = contactInfoPage.email.getText();
+        String actualPhone = contactInfoPage.phone.getText();
+
+        System.out.println("actualFullName = " + actualFullName);
+        System.out.println("actualEmail = " + actualEmail);
+        System.out.println("actualPhone = " + actualPhone);
+
+        //get information from database
+
+        //we are getting only one row of result
+        //query for retrieving firstname,lastname,email,phone
+        String query ="select concat(first_name,' ',last_name) as \"full_name\",e.email,phone\n" +
+                "from orocrm_contact c join orocrm_contact_email e\n" +
+                "on c.id = e.owner_id join orocrm_contact_phone p\n" +
+                "on e.owner_id = p.owner_id\n" +
+                "where e.email='"+email+"'";
+        //get info and save in the map
+        Map<String, Object> rowMap = DBUtils.getRowMap(query);
+
+        String expectedFullName = (String) rowMap.get("full_name");
+        String expectedPhone = (String) rowMap.get("phone");
+        String expectedEmail = (String) rowMap.get("email");
+
+        System.out.println("expectedFullName = " + expectedFullName);
+        System.out.println("expectedPhone = " + expectedPhone);
+        System.out.println("expectedEmail = " + expectedEmail);
+
+
+        //assertion, Compare UI against to DB
+        Assert.assertEquals(expectedFullName,actualFullName);
+        Assert.assertEquals(expectedEmail,actualEmail);
+        Assert.assertEquals(expectedPhone,actualPhone);
+
+
+    }
 
 
 }
--- src/test/resources/features/Contacts.feature
+++ src/test/resources/features/Contacts.feature
@@ -61,9 +61,28 @@ Feature: Contacts page
       | user10         | Brenden   | Schneider |
       | storemanager85 | Stephan   | Haley     |
 
-    @wip @db
+     @db
     Scenario: Contacts test with email
       Given the user logged in as "sales manager"
       And the user navigates to "Customers" "Contacts"
       When the user clicks the "mbrackstone9@example.com" from contacts
       Then the information should be same with database
+
+  @wip @db
+  Scenario: Contacts test with email
+    Given the user logged in as "sales manager"
+    And the user navigates to "Customers" "Contacts"
+    When the user clicks the "mike.jorden@hotmail.com" from contacts
+    Then the information for "mike.jorden@hotmail.com" should be same with database
+
+
+    Scenario Outline: Contacts Test
+      Given the user logged in as "sales manager"
+      And the user navigates to "Customers" "Contacts"
+      When the user clicks the "<email>" from contacts
+      Then the information for "<email>" should be same with database
+
+      Examples:
+      |email|
+      |mbrackstone9@example.com|
+      |mike.jorden@hotmail.com |
